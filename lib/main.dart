a70b8c912b9770141cf51b76fa9f330ab2142274
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-01-08 23:43:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aditya_portfolio/theme/app_colors.dart';
+import 'theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
